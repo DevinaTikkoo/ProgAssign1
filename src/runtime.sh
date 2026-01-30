62031830bc6cd4a67fa6a3d7270332498984e1db
namtran1812 <158846154+namtran1812@users.noreply.github.com>
2026-01-30 10:39:26 -0500

    Add runtime.sh and output files

---
+++ src/runtime.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+n=(1 2 4 8 16 32 64 128 256 512 1024 2048)
+for i in "${n[@]}"
+do
+   echo "Measuring time for n=$i"
+   input_file="tests/example_$i.in"
+   output_file="tests/example_$i.out"
+   #matcher
+   echo "matcher:"
+   time python src/matcher.py < "$input_file" > "$output_file"
+   #verifier
+   echo "verifier:"
+   time python src/verifier.py "$input_file" "$output_file"
+done
+#chmod +x src/runtime.sh
+#./src/runtime.sh
